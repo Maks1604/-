0860675f06cb579879a9b7b325e85988566d9ca3
maks1604 <maks16041991@gmail.com>
2019-05-15 22:37:49 +0300

    1 Задание рекурсия

--- Lesson5/src/Lesson5/Main.java
+++ Lesson5/src/Lesson5/Main.java
@@ -3,6 +3,23 @@ package Lesson5;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(powerRec(3,5));
+    }
+
+    public static long powerRec(int num, int pow) {
+
+        if (pow == 0) {
+            return  1;
+        } else if (pow == 1) {
+            return num;
+        } else if (pow % 2 == 0){
+           long a = powerRec(num,pow/2);
+           return a*a;
+        } else {
+            long a = powerRec(num,pow/2);
+            return a*a*num;
+        }
+
+
     }
 }
