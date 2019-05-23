bd39930ccf2a4f95b5524a3429bd34bd95496563
maks1604 <maks16041991@gmail.com>
2019-05-24 00:12:43 +0300

    Готово

--- Lesson7/src/Lesson7/Main.java
+++ Lesson7/src/Lesson7/Main.java
@@ -3,45 +3,40 @@ package Lesson7;
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(13);
-        /*graph.addEdge(0, 6);
-        graph.addEdge(0, 1);
+        Graph graph = new Graph(10);
         graph.addEdge(0, 2);
-        graph.addEdge(5, 3);
-        graph.addEdge(5, 4);
+        graph.addEdge(0, 1);
+        graph.addEdge(2, 1);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 7);
         graph.addEdge(5, 0);
-        graph.addEdge(6, 4);
-        graph.addEdge(3, 4);
+        graph.addEdge(7, 4);
+        graph.addEdge(4, 3);
 
-        graph.addEdge(7, 8);
+        graph.addEdge(3, 5);
 
-        graph.addEdge(9, 10);
-        graph.addEdge(9, 12);
-        graph.addEdge(9, 11);
-        graph.addEdge(11, 12);
+        graph.addEdge(5, 6);
+        graph.addEdge(6, 7);
+        graph.addEdge(7, 2);
+        graph.addEdge(2, 9);
+        graph.addEdge(9, 8);
+        graph.addEdge(8, 0);
 
         DepthFirstPaths dfs = new DepthFirstPaths(graph, 0);
 
         System.out.println(dfs.hasPathTo(5));
-        System.out.println(dfs.hasPathTo(7));
-        System.out.println(dfs.hasPathTo(11));
+        System.out.println(dfs.hasPathTo(8));
+        System.out.println(dfs.hasPathTo(9));
         if (dfs.hasPathTo(5)) {
             System.out.println(dfs.pathTo(5));
-        }*/
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 1);
-        graph.addEdge(1, 2);
-        graph.addEdge(3, 5);
-        graph.addEdge(3, 4);
-        graph.addEdge(3, 2);
-        graph.addEdge(4, 2);
-        graph.addEdge(5, 0);
+        }
+
 
         BreadthFirstSearch bfs = new BreadthFirstSearch(graph, 0);
 
-        System.out.println(bfs.hasPathTo(3));
-        System.out.println(bfs.pathTo(3));
-        System.out.println(bfs.distTo(3));
+        System.out.println(bfs.hasPathTo(9));
+        System.out.println(bfs.pathTo(9));
+        System.out.println(bfs.distTo(9));
 
     }
 }
